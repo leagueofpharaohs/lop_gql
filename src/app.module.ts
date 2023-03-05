2b5179434f6a63504c22e7e4e583dbf6ef59daeb
mazenalhabeby <a.mazen.dessouky@gmail.com>
2023-03-05 16:55:57 +0100

    edit redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,10 @@ import { BalanceModule } from './balance/balance.module';
     RedisModule.forRootAsync({
       useFactory: () => ({
         config: {
-          url: process.env.REDIS_URL,
+          host: process.env.REDIS_HOST,
+          port: Number(process.env.REDIS_PORT),
+          password: process.env.REDIS_PASSWORD,
+          username: process.env.REDIS_USERNAME,
         },
       }),
     }),
