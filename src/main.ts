a2fa2f4f60d8c09b0c0477d99ef77ee69fcf1dd2
mazenalhabeby <a.mazen.dessouky@gmail.com>
2023-03-08 09:36:27 +0100

    edit port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(8080);
+  await app.listen(3333);
 }
 bootstrap();
